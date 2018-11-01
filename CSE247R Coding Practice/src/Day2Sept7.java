f70b4232dea18bd6596e551454de92b426bc791c
Pyi Theim Kyaw <k.pyitheim@wustl.edu>
2018-11-01 12:38:20 -0500

    updated

---
+++ CSE247R Coding Practice/src/Day2Sept7.java
@@ -0,0 +1,9 @@
+
+public class Day2Sept7 extends Day1Aug31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CSE247R Coding Practice/src/Day3Spet14.java
@@ -0,0 +1,13 @@
+import javax.xml.soap.Node;
+
+public class Day3Spet14 {
+	
+	public static Node reverseLL (Node head) {
+		return head;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
